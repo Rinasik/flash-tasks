c56013aa9b3b893b98cb0859efee81ff8dff329e
Rinasik <mubarakshin199999@mail.ru>
2022-05-15 15:58:22 +0300

    Add new baseUrl

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { authService } from "../services/authService";
 
 export const api = axios.create({
-  baseURL: "http://localhost:9000/api/",
+  baseURL: "http://51.250.105.230/",
 });
 
 api.interceptors.request.use((config: any) => {
